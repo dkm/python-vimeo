cab705e2b78b9fd0665814e6accabe126700646e
marc <marc@etiquette.(none)>
2010-02-08 15:48:28 +0100

    cleaned cli args for user/video/channel IDs

--- bin/vimeo-query.py
+++ bin/vimeo-query.py
@@ -29,6 +29,9 @@ import sys
 import optparse
 import xml.etree.ElementTree as ET
 
+
+
+
 def main(argv):
     parser = optparse.OptionParser(
         usage='Usage: %prog [options]',
@@ -45,28 +48,6 @@ def main(argv):
                       help="Access token secret")
     parser.add_option('-v', '--verifier',
                       help="Verifier for token")
-    parser.add_option('--quota',
-                      help="Get user quota", action="store_true", default=False)
-    parser.add_option('--get-channels',
-                      help="Get all public channels", action="store_true", default=False)
-    parser.add_option('--get-channel-info', metavar="CHANNEL_ID",
-                      help="Get info on a specific channel")
-    parser.add_option('--get-video-info', metavar="VID_ID",
-                      help="Get info on a specific video")
-    parser.add_option('--page', metavar="NUM",
-                      help="Page number, when it makes sense...")
-    parser.add_option('--per-page', metavar="NUM",
-                      help="Per page number, when it makes sense...")
-    parser.add_option('--sort', metavar="SORT",
-                      help="sort order, when it makes sense (accepted values depends on the query)...")
-    parser.add_option('--get-channel-moderators', metavar="CHAN_ID",
-                      help="Get moderators for a specific channel")
-    parser.add_option('--get-channel-subscribers', metavar='CHAN_ID',
-                      help="Get subscribers for a specific channel")
-    parser.add_option('--get-channel-videos', metavar='CHAN_ID',
-                      help="Get videos for a specific channel")
-    parser.add_option('--get-contacts', metavar='USER_ID',
-                      help="Get all contacts for a specific user")
 
     parser.add_option('--album', metavar="ALBUM_ID",
                       action="append",
@@ -76,27 +57,79 @@ def main(argv):
                       action="append",
                       help="Specify on which channel other command acts."
                            +"Can be used more than once")
+    parser.add_option('--video', metavar="VIDEO_ID",
+                      action="append",
+                      help="Specify on which video other command acts."
+                           +"Can be used more than once")
+    parser.add_option('--user', metavar="USER_ID",
+                      action="append",
+                      help="Specify on which user other command acts."
+                           +"Can be used more than once")
 
-    parser.add_option('--add-video', metavar='VIDEO_ID',
-                      help="Add the given video to the album(s) and channel(s)" +
-                           "specified with --album and --channel argument.")
+    parser.add_option('--quota',
+                      help="Get user quota", action="store_true", default=False)
 
-    parser.add_option('--remove-video', metavar='VIDEO_ID',
-                      help="Remove the given video from the album(s) and channel(s)" +
-                           "specified with --album and --channel argument.")
+    parser.add_option('--get-channels',
+                      help="Get all public channels", action="store_true", default=False)
+    parser.add_option('--get-channel-info',
+                      help="Get info on channel CHANNEL_ID",
+                      action="store_true")
+
+    parser.add_option('--get-video-info',
+                      help="Get info on video VIDEO_ID",
+                      action="store_true")
+
+    parser.add_option('--page', metavar="NUM",
+                      help="Page number, when it makes sense...")
+    parser.add_option('--per-page', metavar="NUM",
+                      help="Per page number, when it makes sense...")
+    parser.add_option('--sort', metavar="SORT",
+                      help="sort order, when it makes sense (accepted values depends on the query)...")
+    parser.add_option('--get-channel-moderators',
+                      help="Get moderators for channel CHANNEL_ID",
+                      action="store_true")
+    parser.add_option('--get-channel-subscribers',
+                      help="Get subscribers for channel ChANNel_ID",
+                      action="store_true")
+    parser.add_option('--get-channel-videos',
+                      help="Get videos for channel CHANNEL_ID",
+                      action="store_true")
+    parser.add_option('--get-contacts',
+                      help="Get all contacts for user USER_ID",
+                      action="store_true")
+
+
+    parser.add_option('--add-video',
+                      help="Add the video VIDEO_ID to the album ALBUM_ID and channel" +
+                           "CHANNEL_ID.",
+                      action="store_true")
+
+    parser.add_option('--remove-video', 
+                      help="Remove the video ViDEO_ID from the album ALBUM_ID and channel" +
+                           "CHANNEL_ID.",
+                      action="store_true")
 
     parser.add_option('--set-album-description', metavar='DESCRIPTION',
-                      help="Set the description for the album specified with --album")
+                      help="Set the description for the album ALBUM_ID")
 
     parser.add_option('--set-channel-description', metavar='DESCRIPTION',
-                      help="Set the description for the channel specified with --channel")
+                      help="Set the description for the channel CHANNEL_ID")
 
     parser.add_option('--set-password', metavar='PASSWORD',
                       help="Set the password for the channel(s), album(s) and video(s) specified with --channel, --album and --video")
-
-
+    
 
     (options, args) = parser.parse_args(argv[1:])
+
+
+    def check_user():
+        return options.user != None
+
+    def check_channel():
+        return options.channel != None
+            
+    def check_album():
+        return options.album != None
 
     vconfig = vimeo.config.VimeoConfig(options)
 
@@ -126,90 +159,137 @@ def main(argv):
         for channel in channels.findall("channels/channel"):
             print "Name (%s):" %channel.attrib['id'], channel.find('name').text
     
-    elif options.get_channel_info != None:
-        info = client.vimeo_channels_getInfo(options.get_channel_info).find('channel')
-        for text_item in ['name', 'description', 'created_on', 'modified_on', 'total_videos',
-                          'total_subscribers', 'logo_url', 'badge_url', 'url', 'featured_description']:
+    elif options.get_channel_info :
+        if not check_channel():
+            print "Missing channel"
+            parser.print_help()
+            sys.exit(-1)
+        for chan in options.channel:
+            info = client.vimeo_channels_getInfo(chanl).find('channel')
+            for text_item in ['name', 'description', 'created_on', 'modified_on', 'total_videos',
+                              'total_subscribers', 'logo_url', 'badge_url', 'url', 'featured_description']:
                           
-            it = info.find(text_item)
-            if it != None:
-                print "%s:" %text_item, info.find(text_item).text
-        creator = info.find('creator')
-        print "Creator: %s (%s)" %(creator.attrib['display_name'], creator.attrib['id'])
+                it = info.find(text_item)
+                if it != None:
+                    print "%s:" %text_item, info.find(text_item).text
+            creator = info.find('creator')
+            print "Creator: %s (%s)" %(creator.attrib['display_name'], creator.attrib['id'])
        
-    elif options.get_video_info != None:
-        info = client.vimeo_videos_getInfo(options.get_video_info)
+    elif options.get_video_info:
+        if not check_video():
+            print "Missing video"
+            parser.print_help()
+            sys.exit(-1)
+
+        for vid in options.video:
+            info = client.vimeo_videos_getInfo(vid)
 
     
-    elif options.get_channel_moderators != None:
-        moderators = client.vimeo_channels_getModerators(options.get_channel_moderators,
-                                                         page=options.page,
-                                                         per_page=options.per_page)
-        for moderator in moderators.findall('moderators/user'):
-            print "Name: %s (%s)" %(moderator.attrib['display_name'], moderator.attrib['id'])
+    elif options.get_channel_moderators:
+        if not check_channel():
+            print "Missing channel"
+            parser.print_help()
+            sys.exit(-1)
 
-    elif options.get_channel_subscribers != None:
-        subs = client.vimeo_channels_getSubscribers(options.get_channel_subscribers,
-                                                    page=options.page,
-                                                    per_page=options.per_page)
-        for sub in subs.findall('subscribers/subscriber'):
-            print "Name: %s (%s)" %(sub.attrib['display_name'], sub.attrib['id'])
+        for chan in options.channel:
+            moderators = client.vimeo_channels_getModerators(chan,
+                                                             page=options.page,
+                                                             per_page=options.per_page)
+            for moderator in moderators.findall('moderators/user'):
+                print "Name: %s (%s)" %(moderator.attrib['display_name'], moderator.attrib['id'])
+
+    elif options.get_channel_subscribers:
+        if not check_channel():
+            print "Missing channel"
+            parser.print_help()
+            sys.exit(-1)
+
+        for chan in options.channel:
+            subs = client.vimeo_channels_getSubscribers(chan,
+                                                        page=options.page,
+                                                        per_page=options.per_page)
+            for sub in subs.findall('subscribers/subscriber'):
+                print "Name: %s (%s)" %(sub.attrib['display_name'], sub.attrib['id'])
 
     elif options.get_channel_videos != None:
-        vids = client.vimeo_channels_getVideos(options.get_channel_videos,
-                                               page=options.page,
-                                               per_page=options.per_page)
-        for vid in vids.findall('videos/video'):
-            print "Video: %s (%s), uploaded %s" %(vid.attrib['title'], 
-                                                  vid.attrib['id'], 
-                                                  vid.attrib['upload_date'])
+        if not check_channel():
+            print "Missing channel"
+            parser.print_help()
+            sys.exit(-1)
+
+        for chan in options.channel:
+            vids = client.vimeo_channels_getVideos(chan,
+                                                   page=options.page,
+                                                   per_page=options.per_page)
+            for vid in vids.findall('videos/video'):
+                print "Video: %s (%s), uploaded %s" %(vid.attrib['title'], 
+                                                      vid.attrib['id'], 
+                                                      vid.attrib['upload_date'])
     elif options.get_contacts:
-        contacts = client.vimeo_contacts_getAll(options.get_contacts,
-                                                sort=options.sort,
-                                                page=options.page,
-                                                per_page=options.per_page)
-        for contact in contacts.findall('contacts/contact'):
-            print "Contact: %s (%s)" %(contact.attrib['display_name'], contact.attrib['id'])
+        if not check_user():
+            print "Missing user"
+            parser.print_help()
+            sys.exit(-1)
+
+        for user in options.user:
+            contacts = client.vimeo_contacts_getAll(user,
+                                                    sort=options.sort,
+                                                    page=options.page,
+                                                    per_page=options.per_page)
+            for contact in contacts.findall('contacts/contact'):
+                print "Contact: %s (%s)" %(contact.attrib['display_name'], contact.attrib['id'])
 
     elif options.add_video:
-        if options.album:
-            for alb in options.album:
-                client.vimeo_albums_addVideo(options.add_video,
-                                             alb)
-        if options.channel:
-            for chan in options.channel:
-                client.vimeo_channels_addVideo(options.add_video,
-                                               chan)
+        if not check_video():
+            print "Missing video"
+            parser.print_help()
+            sys.exit(-1)
+
+        for vid in options.video:
+            if options.album:
+                for alb in options.album:
+                    client.vimeo_albums_addVideo(vid,
+                                                 alb)
+            if options.channel:
+                for chan in options.channel:
+                    client.vimeo_channels_addVideo(vid,
+                                                   chan)
 
     elif options.remove_video:
-        if options.album:
-            for alb in options.album:
-                client.vimeo_albums_removeVideo(options.remove_video,
+        if not check_video():
+            print "Missing video"
+            parser.print_help()
+            sys.exit(-1)
+
+        for vid in options.video:
+            if options.album:
+                for alb in options.album:
+                    client.vimeo_albums_removeVideo(vid,
                                                 alb)
-        if options.channel:
-            for chan in options.channel:
-                client.vimeo_channels_removeVideo(options.remove_video,
-                                                  chan)
+            if options.channel:
+                for chan in options.channel:
+                    client.vimeo_channels_removeVideo(vid,
+                                                      chan)
 
     elif options.set_album_description:
-        if options.album:
-            for alb in options.album:
-                client.vimeo_albums_setDescription(options.set_album_description,
-                                                   alb)
-        else:
-            print "Missing album(s) for the set-album-description command"
+        if not check_album():
+            print "Missing album"
             parser.print_help()
             sys.exit(-1)
 
+        for alb in options.album:
+            client.vimeo_albums_setDescription(options.set_album_description,
+                                               alb)
+
     elif options.set_channel_description:
-        if options.channel:
-            for chan in options.channel:
-                client.vimeo_channels_setDescription(options.set_channel_description,
-                                                     chan)
-        else:
-            print "Missing channel(s) for the set-channel-description command"
+        if not check_channel():
+            print "Missing channel"
             parser.print_help()
             sys.exit(-1)
+
+        for chan in options.channel:
+            client.vimeo_channels_setDescription(options.set_channel_description,
+                                                 chan)
 
     elif options.set_password:
         if options.channel:
@@ -226,7 +306,6 @@ def main(argv):
                                                password=options.set_password,
                                                video_id=vid)
 
-        
 
 if __name__ == '__main__':
     main(sys.argv)
